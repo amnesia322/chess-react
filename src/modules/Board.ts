489c21e44e3f87509c079cd8af83a292d4a48e9d
Sergey Molchanov <molchanov.sergei@neuro-city.ru>
2023-03-15 15:14:43 +0300

    feat: add figures

---
+++ src/modules/Board.ts
@@ -0,0 +1,49 @@
+import {Cell} from "./Cell";
+import {Colors} from "./Colors";
+import {Queen} from "./figures/Queen";
+
+export class Board {
+    cells: Cell[][] = [];
+
+    public initCells () {
+        for (let i = 0; i < 8; i++) {
+            const row: Cell[] = []
+            for (let j = 0; j < 8; j++) {
+                if ((i + j) % 2 !== 0) {
+                    row.push(new Cell(this, j, i, Colors.BLACK, null)) // black cells
+                } else {
+                    row.push(new Cell(this, j, i, Colors.WHITE, null)) // white cells
+                }
+            }
+            this.cells.push(row)
+        }
+    }
+
+    public getCell(x: number, y: number) {
+        return this.cells[y][x];
+    }
+
+    private addPawns() {
+
+    }
+
+    private addKings() {
+
+    }
+
+    private addQueens() {
+
+    }
+
+    private addKnights() {
+
+    }
+
+    private addBishops() {
+
+    }
+
+    public addFigures() {
+        new Queen(Colors.WHITE, this.getCell(3,3))
+    }
+}
